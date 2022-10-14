cf6fe275c76fe8a676ca531d1f7d0cc117318685
mmparadinha <matheusmparadinha@gmail.com>
2022-10-14 17:40:12 -0300

    Add redirect short URL to original URL feature

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -16,11 +16,57 @@ export async function shortenUrl(req, res) {
         const shortUrl = await nanoid();
 
         await connection.query(`
-           INSERT INTO urls ("userId", "originalURL", "shortURL") VALUES ($1, $2, $3)
+           INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3)
         ;`, [loggedIn.rows[0].userId, url, shortUrl]);
         return res.status(201).send({
             shortUrl
         });
+
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
+
+export async function listSingleUrl(req, res) {
+    const { id } = req.params;
+
+    try {
+        const urlData = await connection.query(`
+            SELECT
+                urls.id,
+                urls."shortUrl",
+                urls.url
+            FROM urls
+            WHERE id=$1
+        ;`, [id]);
+        if (!urlData.rows[0]) { return res.sendStatus(404) };
+
+        return res.status(200).send(urlData.rows[0]);
+
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
+
+export async function redirectUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const originalUrl = await connection.query(`
+            SELECT
+                urls.id,
+                urls.url
+            FROM urls
+            WHERE "shortUrl"=$1
+        ;`, [shortUrl]);
+        if (!originalUrl.rows[0]) { return res.sendStatus(404) };
+
+        await connection.query(`
+           INSERT INTO visits ("urlId") VALUES ($1)
+        ;`, [originalUrl.rows[0].id]);
+        return res.redirect(originalUrl.rows[0].url);
 
     } catch (err) {
         console.error(err);
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { shortenUrl } from "../controllers/urlsController.js";
+import { shortenUrl, listSingleUrl, redirectUrl } from "../controllers/urlsController.js";
 import { urlMiddleware } from "../middlewares/urlMiddleware.js";
 
 const urlsRouter = express.Router();
 
 urlsRouter.post("/url/shorten", urlMiddleware, shortenUrl);
+urlsRouter.get("/urls/:id", listSingleUrl);
+urlsRouter.get("/urls/open/:shortUrl", redirectUrl);
 
 export default urlsRouter;
