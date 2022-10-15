54c64c57e443e5b8dce1a1477196f45922e5ffb5
mmparadinha <matheusmparadinha@gmail.com>
2022-10-14 21:17:52 -0300

    Add delete shortened URL feature

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -3,9 +3,11 @@ import connection from '../database/database.js';
 import { nanoid } from 'nanoid/async';
 
 export async function shortenUrl(req, res) {
-    const authorization = stripHtml(req.headers.authorization).result;
     const url = stripHtml(req.body.url).result;
-    const token = authorization?.replace('Bearer ', '');
+    const { authorization } = req.headers;
+    if (!authorization) { return res.sendStatus(401) };
+
+    const token = stripHtml(authorization.replace('Bearer ', '')).result;
 
     try {
         const loggedIn = await connection.query(`
@@ -67,6 +69,41 @@ export async function redirectUrl(req, res) {
            INSERT INTO visits ("urlId") VALUES ($1)
         ;`, [originalUrl.rows[0].id]);
         return res.redirect(originalUrl.rows[0].url);
+
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
+
+export async function deleteUrl(req, res) {
+    const { id } = req.params;
+    const { authorization } = req.headers;
+    if (!authorization) { return res.sendStatus(401) };
+
+    const token = stripHtml(authorization.replace('Bearer ', '')).result;
+
+    try {
+        const urlExists = await connection.query(`
+            SELECT * FROM urls WHERE id=$1
+        ;`, [id]);
+        if (!urlExists.rows[0]) { return res.sendStatus(404) };
+
+        const urlOwner = await connection.query(`
+            SELECT 
+                urls.id,
+                urls."userId",
+                sessions.token
+            FROM urls
+            JOIN sessions ON urls."userId"=sessions.id
+            WHERE urls."userId"=$1 AND sessions.token=$2
+        ;`, [id, token]);
+        if (!urlOwner.rows[0]) { return res.sendStatus(401) };
+
+        await connection.query(`
+            DELETE FROM urls WHERE id=$1
+        `, [id]);
+        return res.sendStatus(204);
 
     } catch (err) {
         console.error(err);
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { shortenUrl, listSingleUrl, redirectUrl } from "../controllers/urlsController.js";
+import { shortenUrl, listSingleUrl, redirectUrl, deleteUrl } from "../controllers/urlsController.js";
 import { urlMiddleware } from "../middlewares/urlMiddleware.js";
 
 const urlsRouter = express.Router();
@@ -7,5 +7,7 @@ const urlsRouter = express.Router();
 urlsRouter.post("/url/shorten", urlMiddleware, shortenUrl);
 urlsRouter.get("/urls/:id", listSingleUrl);
 urlsRouter.get("/urls/open/:shortUrl", redirectUrl);
+urlsRouter.delete("/urls/:id", deleteUrl);
+
 
 export default urlsRouter;
